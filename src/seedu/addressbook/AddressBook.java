e74a39a708664bbd33b96188530aeffe0a37ac32
WANGCE\WangC <wangce999@hotmail.com>
2017-01-30 16:48:41 +0800

    replaced magic numbers in isPersonExtractableFrom method

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -957,10 +957,10 @@ public class AddressBook {
     private static boolean isPersonDataExtractableFrom(String personData) {
         final String matchAnyPersonDataPrefix = PERSON_DATA_PREFIX_PHONE + '|' + PERSON_DATA_PREFIX_EMAIL;
         final String[] splitArgs = personData.trim().split(matchAnyPersonDataPrefix);
-        return splitArgs.length == 3 // 3 arguments
-                && !splitArgs[0].isEmpty() // non-empty arguments
-                && !splitArgs[1].isEmpty()
-                && !splitArgs[2].isEmpty();
+        return splitArgs.length == PERSON_DATA_COUNT // 3 arguments
+        	&& !splitArgs[PERSON_DATA_INDEX_NAME].isEmpty() // non-empty arguments
+        	&& !splitArgs[PERSON_DATA_INDEX_PHONE].isEmpty()
+        	&& !splitArgs[PERSON_DATA_INDEX_EMAIL].isEmpty();
     }
 
     /**
